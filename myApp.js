c89cb766df120097020070efc2da6f577a6bf859
Terng Sahasbhop <sahasbhop.suvadhanabhakdi@scbtechx.io>
2022-04-01 17:27:50 +0700

    Install and Set Up Mongoose

--- myApp.js
+++ myApp.js
@@ -1,58 +1,68 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+const mongoUri = process.env.MONGO_URI;
+console.log(`mongoUri: ${mongoUri}`)
 
+mongoose.connect(mongoUri, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result) => {
+        console.log('connected to mongodb successfully')
+    })
+    .catch((error) => {
+        console.log('failed to connect to mongodb')
+    })
 
 let Person;
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const findEditThenSave = (personId, done) => {
-  const foodToAdd = "hamburger";
+    const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const findAndUpdate = (personName, done) => {
-  const ageToSet = 20;
+    const ageToSet = 20;
 
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const removeManyPeople = (done) => {
-  const nameToRemove = "Mary";
+    const nameToRemove = "Mary";
 
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const queryChain = (done) => {
-  const foodToSearch = "burrito";
+    const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 /** **Well Done !!**
-/* You completed these challenges, let's go celebrate !
+ /* You completed these challenges, let's go celebrate !
  */
 
 //----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
